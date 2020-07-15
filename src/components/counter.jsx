20c3b66b9bd565431c59082c649d3b3a8619fedc
Abhishek Kumar <abhishek022kk@gmail.com>
2020-07-15 22:58:48 +0530

    Using counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,19 +3,33 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200",
+  };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    this.getBadgeClass();
+
     return (
       <React.Fragment>
-        <span className={classes}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
+        <button
+          className="btn btn-secondary btn-sm m-2"
+          onClick={this.handleIncrement}
+        >
+          Increment
+        </button>
       </React.Fragment>
     );
   }
+  getBadgeClass() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     return this.state.count === 0 ? "Zero" : this.state.count;
   }
