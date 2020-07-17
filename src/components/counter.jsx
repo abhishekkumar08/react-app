d574a4bb0aee2d04dcb853a1b9ecf1a10208e0d6
Abhishek Kumar <abhishek022kk@gmail.com>
2020-07-17 13:55:38 +0530

    Raising and Handling Events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,35 +2,37 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
   };
 
   handleIncrement = (product) => {
     console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
     return (
-      <React.Fragment>
+      <div>
+    
         <span className={this.getBadgeClass()}>{this.formatCount()}</span>
         <button
           className="btn btn-secondary btn-sm m-2"
-          onClick={ (product) => this.handleIncrement(product) }
+          onClick={(product) => this.handleIncrement(product)}
         >
           Increment
         </button>
-      </React.Fragment>
+        <button onClick={this.props.onDelete} className="btn btn-danger btn-sm m-2">Delete</button>
+      </div>
     );
   }
   getBadgeClass() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
